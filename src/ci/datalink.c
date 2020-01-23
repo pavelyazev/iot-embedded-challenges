7c6286b8df7d77b4878e079ccc9a68f9e427ac76
Pavel Yazev <pyazev@gmail.com>
2020-01-23 11:44:28 +0100

    Data link interface improved

--- src/ci/datalink.c
+++ src/ci/datalink.c
@@ -14,3 +14,14 @@ void dataLinkWriteData(char *data, uint32_t len)
 
 }
 
+
+uint32_t dataLinkIsDataAvailable(void)
+{
+    return 0;
+}
+
+
+uint32_t dataLinkReadData(char *buf, uint32_t bufSize)
+{
+    return 0;
+}
--- src/ci/datalink.h
+++ src/ci/datalink.h
@@ -14,5 +14,18 @@ void dataLinkInit(void);
 */
 void dataLinkWriteData(char *data, uint32_t len);
 
+/** 
+*    @brief Send message to the Communication interface message queue
+*    @retval 1 - if data available, 0 - otherwise
+*/
+uint32_t dataLinkIsDataAvailable(void);
+
+/** 
+*    @brief Get data from data link buffer
+*    @param buf      Pointer to store data
+*    @param bufSize  Max number bytes to be read
+*    @retval  Actual number of bytes read
+*/
+uint32_t dataLinkReadData(char *buf, uint32_t bufSize);
 
 #endif
